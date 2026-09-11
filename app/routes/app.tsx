debbd4e29f880ed484f099dd7aa90d06ad4c7e7d
dr-mohamad-khaled <dr-mohamad-khaled@users.noreply.github.com>
2026-09-12 02:43:11 +0300

    Ensure fallback apiKey in app loader

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -10,7 +10,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   await authenticate.admin(request);
 
   // eslint-disable-next-line no-undef
-  return { apiKey: process.env.SHOPIFY_API_KEY || "" };
+  return { apiKey: process.env.SHOPIFY_API_KEY || "872f7f6415d1c243c11ccdfe9426b07f" };
 };
 
 export default function App() {
